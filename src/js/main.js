3165446d8fcea3dc5ea507b0774583f9a384d3ee
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-10-22 18:47:18 +0300

    Адаптивная шапка сайта

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import windowActionScroll from './modules/windowActionsScroll';
+import burgerAction from './modules/burgerAction';
 import features from './modules/features';
 
 window.addEventListener('DOMContentLoaded', () => {
     windowActionScroll();
+    burgerAction();
     features(['Web Designer', 'Freelancer', 'Front End Developer op!']);
 });
