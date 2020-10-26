a2b3d537292bcc416806e70d95c523c742fd8ac3
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-10-26 22:01:19 +0300

    version 1.0

--- src/js/modules/utils.js
+++
@@ -1,7 +0,0 @@
-export const removeClass = (selector, dom = document) => {
-    dom.querySelectorAll('.' + selector).forEach(item => item.classList.remove(selector));
-};
-
-export const removeSelector = (selector, dom = document) => {
-    dom.querySelectorAll(selector).forEach(item => item && item.remove());
-}
